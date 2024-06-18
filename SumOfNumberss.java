2675892f803a1958c44aa5e986f0fa621179316c
siobhanscott <scottscott1800@gmail.com>
2024-06-18 02:19:33 -0400

    Add files via upload

---
+++ SumOfNumberss.java
@@ -0,0 +1,44 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package sum.of.numbers;
+
+import java.util.Scanner;
+
+/**
+ *
+ * @author Siobhan Scott
+ */
+public class SumOfNumberss {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        
+        Scanner input = new Scanner(System.in); //Create a new Scanner class
+        
+        int n;          // declare variables
+        int sum = 0;
+        
+        
+        System.out.println("Enter a positive integer value");
+        int integer = input.nextInt(); //input user to enter a positive integer value
+        
+        
+        if(!(integer > 0)){
+        System.out.print("Error. Enter a positive integer value ");
+        }
+        
+        for (n = 1; n <= integer; n++ )
+        { //variable n is equal to one. n is less than or equal to the users integer. The "++" operator increases variable n by 1.
+        
+           sum += n; // this can also be written as sum = sum + num which will add the previous value to the current one 
+        }
+        
+        System.out.println("The sum of numbers 1 - " +integer+ " is: " + sum);  //This will print out the sum when the loop is finished.
+    }
+    
+}
+
